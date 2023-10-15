d7e6f4d9e30d513114c4dc30addb51b19c74dcd2
Arjun Sasikumar <itsarjn@gmail.com>
2023-10-15 11:55:21 +0530

    fixed routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,20 @@
+import toast from "react-hot-toast";
 import logoIcon from "../../assets/icons/icon16.png";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+
+  const navigate = useNavigate();
+
+  async function sendTx() {
+    console.log('sendTx');
+    toast.success("Transaction Sent Successfully !", {
+      icon: "🚀",
+      duration: 3000,
+    });
+    navigate(`/dashboard/transaction/add-address`);
+  }
+
   return (
     <>
       <div className=" text-white mt-24 min-h-[210px]">
@@ -11,6 +25,7 @@ function Dashboard() {
           </h2>
         </div>
         <h3 className="text-center text-3xl font-extrabold">20 ETH</h3>
+        <button className="text-center text-sm font-bold underline" onClick={() => { sendTx() }}>Send</button>
       </div>
     </>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "../hooks/useAuth";
 function AppRoutes() {
   const { isLoggedIn } = useAuth();
 
-  return !isLoggedIn ? <PrivateRoutes /> : <PublicRoutes />;
+  return isLoggedIn ? <PrivateRoutes /> : <PublicRoutes />;
 }
 
 export default AppRoutes;
