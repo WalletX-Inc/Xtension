adaecacece9e33e4f9ff88d626405fd3fa0f23d9
“shaktiiii” <shaktidubey@shaktidubey.local>
2023-10-18 00:38:54 +0530

    token card in dashboard

--- src/components/Tab/TabHandler.tsx
+++ src/components/Tab/TabHandler.tsx
@@ -1,6 +1,8 @@
 import Tab from "./";
 import TabContainer from "./Tabs";
 import logoIcon from "../../assets/icons/icon16.png";
+import TokenCard from "../TokenCard";
+import maticLogo from "../../../src/assets/matic-logo.png";
 
 const TabHandler = () => {
   return (
@@ -9,32 +11,13 @@ const TabHandler = () => {
         <Tab label="Tokens">
           <div className="pb-4">
             <h2 className="text-lg font-medium mb-2">All Tokens</h2>
-            <ul className="divide-y border divide-gray-200 dark:divide-gray-700 max-h-[190px] overflow-x-hidden overflow-y-auto">
               {[1, 2, 3].map((token) => (
-                <li className="py-3 px-3 sm:py-4" key={token}>
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="w-8 h-8 rounded-full"
-                        src={logoIcon}
-                        alt="Wallet x"
-                      />
-                    </div>
-                    <div className="flex-1 min-w-0">
-                      <p className="text-sm font-medium truncate dark:text-white">
-                        Neil Sims
-                      </p>
-                      <p className="text-sm truncate dark:text-gray-400">
-                        email@windster.com
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold dark:text-white">
-                      $320
-                    </div>
-                  </div>
-                </li>
+                <TokenCard
+                  tokenIcon={maticLogo}
+                  tokenName="Matic"
+                  tokenBalance={100}
+                />
               ))}
-            </ul>
           </div>
         </Tab>
         <Tab label="NFTs">
--- src/components/TokenCard.tsx
+++ src/components/TokenCard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import maticLogo from "../../src/assets/matic-logo.png";
 
-const TokenCard = () => {
+type tokenCardParams = {
+  tokenIcon:string;
+  tokenName:string;
+  tokenBalance:number;
+}
+
+const TokenCard = ({tokenIcon,tokenName,tokenBalance}:tokenCardParams) => {
   return (
     <div className="flex border item-center py-2 px-3 gap-3 bg-gray-800 rounded-xl text-white border-gray-500 hover:bg-black mt-2">
       <img className="h-8" src={maticLogo} alt="token icon" />
