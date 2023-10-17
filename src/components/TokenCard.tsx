783db70d2fae4f1e5ebb14adbe0c89d32449d10a
“shaktiiii” <shaktidubey@shaktidubey.local>
2023-10-18 00:25:58 +0530

    token card added

--- src/components/TokenCard.tsx
+++ src/components/TokenCard.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import maticLogo from "../../src/assets/matic-logo.png";
 
 const TokenCard = () => {
   return (
-    <div>TokenCard</div>
-  )
-}
+    <div className="flex border item-center py-2 px-3 gap-3 bg-gray-800 rounded-xl text-white border-gray-500 hover:bg-black mt-2">
+      <img className="h-8" src={maticLogo} alt="token icon" />
+      <div className="flex justify-between w-full">
+        <p className="font-semibold tracking-wide">Polygon</p>
+        <p className="font-semibold">100</p>
+      </div>
+    </div>
+  );
+};
 
-export default TokenCard
+export default TokenCard;
