099ce5d0548ffc49334b68c62b2463ab0a5b2d90
“shaktiiii” <shaktidubey@shaktidubey.local>
2023-10-30 15:32:56 +0530

    WIP- please fix the bug guys of useEffect

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,29 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 import { useRecoilState } from "recoil";
 
 import { transferState } from "../../state/TransferState";
-import { generateAddressIcon, getItemFromStorage, getShortDisplayString, getChainDetails } from "../../utils/helper";
+
 import { useConfig } from "../../context/ConfigProvider";
+import { useCoinBalance } from "../../hooks/functional-hooks";
 import Chains from "../../constants/chains";
 import QRCodeModal from "../../components/QRCodeModal";
-import { useCoinBalance } from "../../hooks/functional-hooks"
+import Loader from "../../components/common/Loader";
+import {
+  generateAddressIcon,
+  getItemFromStorage,
+  getShortDisplayString,
+  getChainDetails,
+} from "../../utils/helper";
 
 import swap from "../../assets/swap.png";
 import bridge from "../../assets/rainbow.png";
 import receive from "../../assets/arrow-down.png";
 import send from "../../assets/arrow-up.png";
-
 import copyAndPaste from "../../assets/copy.svg";
-
-import toast, { Toaster } from "react-hot-toast";
-import Loader from "../../components/common/Loader";
+import localforage from "localforage";
 
 function Dashboard() {
   const [transferData, setTransferData] = useRecoilState(transferState);
@@ -26,6 +31,8 @@ function Dashboard() {
   const [currentCoinName, setCurrentCoinName] = useState<string>("");
   const [qrcodemodal, setQrcodemodal] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [currentCoinBalance, setCurrentCoinBalance] = useState<string>();
+  const balanceFromIndexedDB: any = localforage.getItem("coinBalance");
 
   const item = getItemFromStorage("smartAccount");
   const chain = getItemFromStorage("network");
@@ -45,7 +52,115 @@ function Dashboard() {
   const storageChainId = getItemFromStorage("network");
   const chainDetails = getChainDetails(storageChainId);
 
-  const { balance } = useCoinBalance(SCW || smartAccountAddress, true, chainDetails.wssRpc);
+  const { balance } = useCoinBalance(
+    SCW || smartAccountAddress,
+    true,
+    chainDetails.wssRpc
+  );
+
+  const setBalanceInStorage = () => {
+    const key = "coinBalance";
+    localforage.getItem(key).then((coinData: any) => {
+      console.log("The coin Data is #################### ", coinData);
+
+      if (coinData && coinData[chainId]) {
+        console.log(
+          "The balance of current chain &&&&&&&&&&&&&&&&&&&&&&&&&&&&& ",
+          coinData[chainId]
+        );
+
+        const savedBalance = coinData[chainId];
+        console.log(savedBalance);
+
+        // setCurrentCoinBalance(savedBalance);
+        if (balance !== savedBalance) {
+          console.log(
+            "This is in the if state ment ||||||||||||||||||||||||| ",
+            balance,
+            savedBalance
+          );
+
+          // setCurrentCoinBalance(balance);
+          const updatedObject = { ...coinData, [chainId]: balance };
+
+          localforage.setItem(key, updatedObject);
+        }
+      } else {
+        const newObject = { ...coinData, [chainId]: balance };
+        localforage.setItem(key, newObject);
+        // setCurrentCoinBalance(balance);
+      }
+    });
+  };
+
+  useEffect(() => {
+    console.log(
+      "THIS IS THE USE EFFECT FOR BALANCE UPDATE AND SAVE IN THE INDEXED DB"
+    );
+
+    setBalanceInStorage();
+  }, [chainId, balance]);
+
+  /// COIN BALANCE STORING IN  Indexed DB
+
+  // const setCoinBalanceInStorage = async (coinData: any) => {
+  //   let key = "coinBalance";
+
+  //   const coinBalance: any = await localforage.getItem(key);
+  //   if (coinBalance) {
+  //     coinBalance[coinData.chain] = coinData.balance;
+  //     await localforage.setItem(key, coinBalance);
+  //     console.log(coinBalance);
+  //   } else {
+  //     const newCoinBlance = { [coinData.chain]: coinData.balance };
+  //     await localforage.setItem(key, newCoinBlance);
+  //     console.log(newCoinBlance);
+  //   }
+  // };
+
+  // const getAndSetCoinBalanceInStorage = () => {
+  //   let key = "coinBalance";
+  //   localforage.getItem(key).then((balanceData: any) => {
+  //     if (balanceData && balanceData[chainId]) {
+  //       const currentBalanceData: any = balanceData[chainId];
+  //       setCurrentCoinBalance(currentBalanceData); // this will update the current balance form the saved balance in the indexed db
+
+  //       console.log("The balance is ############################", balance);
+  //       console.log(
+  //         "The currentCoinBalance is  ############################",
+  //         currentCoinBalance
+  //       );
+
+  //       if (balance !== currentCoinBalance) {
+  //         setCurrentCoinBalance(balance);
+  //         console.log(
+  //           "balance in getAndSetCoinBalanceInStorage function",
+  //           balance
+  //         );
+
+  //         setCoinBalanceInStorage({ chain: chainId, balance: balance });
+  //       }
+  //     } else {
+  //       console.log("This is the ELSE PART ");
+
+  //       setCoinBalanceInStorage({ chain: chainId, balance: balance });
+  //       setCurrentCoinBalance(balance);
+  //     }
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   console.log("This is balance useEffect");
+  //   getAndSetCoinBalanceInStorage();
+  //   console.log(
+  //     "This is the balance in the use effect of balance",
+  //     currentCoinBalance
+  //   );
+
+  //   return () => {
+  //     setCoinBalanceInStorage({ chain: chainId, balance: balance });
+  //   };
+  // }, [chainId, balance, currentCoinBalance]);
 
   // Receve Button functions
   const openQrModal = () => {
@@ -64,6 +179,10 @@ function Dashboard() {
       console.error("Copy failed due to: ", error);
     }
   };
+
+  async function sendTx() {
+    navigate(`/dashboard/transaction/add-address`);
+  }
 
   useEffect(() => {
     async function initializeSmartWallet() {
@@ -91,10 +210,13 @@ function Dashboard() {
     }
   }, [storageChainId]);
 
-  async function sendTx() {
-    navigate(`/dashboard/transaction/add-address`);
-  }
+  useEffect(() => {
+    // This is to clear the state if the user restarts the app and is on the dashboard.
+    // Optimize it for better UX by using a chorme hook and calling a modal for cancel confirmation.
+    setTransferData([]);
+  }, []);
 
+  // console.log("Current coin balance is ", currentCoinBalance);
   return (
     <>
       <div className=" text-white mt-24 min-h-[210px]">
@@ -116,7 +238,8 @@ function Dashboard() {
           <Toaster position="top-center" reverseOrder={false} />
         </div>
         <h3 className="text-center text-3xl font-extrabold">
-          {(!balance ? 0 : balance)} {currentCoinName}
+          {!balanceFromIndexedDB[chainId] ? 0 : balanceFromIndexedDB[chainId]}{" "}
+          {currentCoinName}
         </h3>
 
         {/* Features Buttons  */}
